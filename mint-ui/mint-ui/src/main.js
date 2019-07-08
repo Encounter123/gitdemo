deac1802878459d22d9a3a83963406ed91b18bf1
ykq <529546920@qq.com>
2019-07-08 14:18:48 +0800

    mint-ui项目初始化

---
+++ mint-ui/mint-ui/src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import MINT from 'mint-ui'
+// import 'mint-ui/lib/style.css'
+import App from './App'
+import router from './router'
+
+Vue.use(MINT)
+
+
+import { Toast, Button, Header } from "mint-ui";
+window.Toast = Toast
+
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ mint-ui/mint-ui/src/views/start.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div @click="btn">这是一个按钮</div>  
+    <mt-button  @click="btn" type="danger">按钮</mt-button>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import {  Button, Header } from "mint-ui";
+Vue.component(Button.name, Button)
+// window.Toast = Toast
+
+/**
+ * 文档地址
+ * @param {type} 
+ * @return: 
+ */
+// Vue文档：https://vue.docschina.org/v2/guide
+// mint-ui文档：http://mint-ui.github.io
+
+export default {
+  data (){
+    return {
+
+    }
+  },
+  components :{
+    // 'mt-button':Button
+  },
+  methods:{
+    btn(){
+      console.log(12)
+      Toast('提示信息')
+    }
+  },
+  created(){
+    Toast('提示信息')
+  }
+}
+</script>
+
